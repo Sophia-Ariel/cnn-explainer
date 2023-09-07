90cab7c96d29b7d063eceeac591c5c99c2dc3be9
Zhou He Sheng <hszhou@ilegendsoft.com>
2023-09-07 13:35:20 +0800

    update deploy scripts

--- src/utils/deploy-to-gh-pages.sh
+++ src/utils/deploy-to-gh-pages.sh
@@ -2,20 +2,22 @@
 set -o errexit
 
 # config
-git config --global user.email "xiao.hk1997@gmail.com"
-git config --global user.name "xiaohk"
+# git config --global user.email "xiao.hk1997@gmail.com"
+# git config --global user.name "xiaohk"
 
 # build
-git clone git@github.com:poloclub/cnn-explainer.git
-cd cnn-explainer
+# git clone git@github.com:poloclub/cnn-explainer.git
+# cd cnn-explainer
 
 npm install
 npm run build
 
 mkdir dist
-copy -r ./public/* ./dist
-sed -i 's/\/assets/\/cnn-explainer\/assets/g' ./dist/index.html
+cp -r ./public/* ./dist
+sed -i '' 's/poloclub/sophia-ariel/g' ./dist/index.html
+# add path prefix cnn-explainer
+# sed -i '' 's/\/assets/\/cnn-explainer\/assets/g' ./dist/index.html 
 
-git add dist
-git commit -m "Deploy gh-pages from Travis"
+git add dist -f
+git commit -m "Deploy gh-pages"
 git subtree push --prefix dist origin gh-pages
